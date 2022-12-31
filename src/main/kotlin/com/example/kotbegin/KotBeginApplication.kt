8eb5e757894b482efd2aefcbccb02f002fbe1b49
NamYoung Kim <095201a@naver.com>
2022-12-31 21:33:41 +0900

    feat : 프로젝트 초기화

---
+++ src/main/kotlin/com/example/kotbegin/KotBeginApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotbegin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotBeginApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotBeginApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotbegin/KotBeginApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotbegin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotBeginApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
